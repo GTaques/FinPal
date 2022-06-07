640fccced91f95d6d0c7b3ee3509d1afe71ee73d
Gabe <gabrielpctaques@gmail.com>
2022-06-07 11:15:31 -0300

    Initial Commit

---
+++ FinPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinPal
+//
+//  Created by Gabriel Taques on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
